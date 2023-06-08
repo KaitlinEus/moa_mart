2a4f22b1cedd5d8884ba3edb34dfdc134a802611
KaitlinEus <131826234+KaitlinEus@users.noreply.github.com>
2023-06-09 11:53:06 +1200

    Created ver 1 and 2 of welcome.py

---
+++ C1/welcome v1.py
@@ -0,0 +1,26 @@
+import random
+from random import randint
+
+# List of random names
+names = ["Yeonjun", "Taehyun", "Huening Kai", "Beomgyu", "Soobin", "Steve", "Daniel ", "Ben", "Terry", "Hyuka"]
+
+num = randint(0,9)
+
+name = (names[num])
+
+# Welcome message
+print(""" 
+
+     __    __     ______     ______          __    __     ______     ______     ______  
+    /\ "-./  \   /\  __ \   /\  __ \        /\ "-./  \   /\  __ \   /\  == \   /\__  _\ 
+    \ \ \-./\ \  \ \ \/\ \  \ \  __ \       \ \ \-./\ \  \ \  __ \  \ \  __<   \/_/\ \/ 
+     \ \_\ \ \_\  \ \_____\  \ \_\ \_\       \ \_\ \ \_\  \ \_\ \_\  \ \_\ \_\    \ \_\ 
+      \/_/  \/_/   \/_____/   \/_/\/_/        \/_/  \/_/   \/_/\/_/   \/_/ /_/     \/_/
+      
+      
+      """)
+print("+×+ One Dream! Hello, we are TOMORROW X TOGETHER! +×+")
+
+print("+×+ My name is",name, "+×+")
+
+print("+×+ I will be here to help you order TXT albums! +×+")
---
+++ C1/welcome v2.py
@@ -0,0 +1,46 @@
+import random
+from random import randint
+
+# List of random names
+names = ["Yeonjun", "Taehyun", "Huening Kai", "Beomgyu", "Soobin", "Steve", "Daniel ", "Ben", "Terry", "Hyuka"]
+
+def welcome():
+
+    '''
+    Purpose: To run all functions
+    Parameters: None
+    Returns: None
+    '''
+
+    num = randint(0,9)
+    name = (names[num])
+
+# Welcome message
+print(""" 
+
+     __    __     ______     ______          __    __     ______     ______     ______  
+    /\ "-./  \   /\  __ \   /\  __ \        /\ "-./  \   /\  __ \   /\  == \   /\__  _\ 
+    \ \ \-./\ \  \ \ \/\ \  \ \  __ \       \ \ \-./\ \  \ \  __ \  \ \  __<   \/_/\ \/ 
+     \ \_\ \ \_\  \ \_____\  \ \_\ \_\       \ \_\ \ \_\  \ \_\ \_\  \ \_\ \_\    \ \_\ 
+      \/_/  \/_/   \/_____/   \/_/\/_/        \/_/  \/_/   \/_/\/_/   \/_/ /_/     \/_/
+      
+      
+      """)
+print("+×+ One Dream! Hello, we are TOMORROW X TOGETHER! +×+")
+
+print("+×+ My name is",name, "+×+")
+
+print("+×+ I will be here to help you order TXT albums! +×+")
+
+def main():
+     
+    '''
+    Purpose: To generate a random name from the list and print out a welcome message
+    Parameters: None
+    Returns: None
+    '''
+     
+    welcome()
+
+
+main()
