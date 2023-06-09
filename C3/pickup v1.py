16491e8c66d6ca4c3a0c960ad6c275752b9573dd
KaitlinEus <131826234+KaitlinEus@users.noreply.github.com>
2023-06-09 12:33:14 +1200

    Created pickup.py
    4 vers.
    Asks user for their names and number

---
+++ C3/pickup v1.py
@@ -0,0 +1,12 @@
+#bug - accepts blank intput - accepts all characters
+
+print("Please enter the pickup information")
+
+# customer name
+name = input("Please enter your name ")
+
+# customer phone number
+phone = input("Please enter your phone number ")
+
+print(name)
+print(phone)
---
+++ C3/pickup v4.py
@@ -0,0 +1,22 @@
+# customer detailes dictionary
+customer_details = {}
+
+def not_blank(question):
+    valid = False
+    while not valid: 
+        response = input(question)
+        if response != "":
+            return response
+        else:
+            print("+×+ Sorry this cannot be blank +×+")
+
+# basic intstructions
+question = ("+×+ Please enter your name +×+ ")
+customer_details['name'] = not_blank(question)
+
+print("+×+ ", customer_details["name"]," +×+")
+
+question = ("+×+ Please enter your phone number +×+ ")
+customer_details['phone'] = not_blank(question)
+
+print("+×+ ",customer_details["phone"]," +×+")
