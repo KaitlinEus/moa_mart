31dcc90a61586902688842f8ba9d9b901f185d66
KaitlinEus <131826234+KaitlinEus@users.noreply.github.com>
2023-06-11 22:13:25 +1200

    Created delivery code
    code so users can input delivery info

--- C4/delivery v2.py
+++ C4/delivery v2.py
@@ -12,27 +12,27 @@ def not_blank(question):
 
 def delivery_info():
 
-    question = ("+×+ PPlease enter your name +×+ P")
+    question = ("+×+ Please enter your name +×+ ")
     customer_details['name'] = not_blank(question)
 
     print(customer_details["name"])
 
-    question = ("+×+ PPlease enter your phone number +×+ P")
+    question = ("+×+ Please enter your phone number +×+ ")
     customer_details['phone'] = not_blank(question)
 
     print(customer_details["phone"])
 
-    question = ("+×+ PPlease enter your house number +×+ P")
+    question = ("+×+ Please enter your house number +×+ ")
     customer_details['house'] = not_blank(question)
 
     print(customer_details['house'])
 
-    question = ("+×+ PPlease enter your street name +×+ P")
+    question = ("+×+ Please enter your street name +×+ ")
     customer_details['street'] = not_blank(question)
 
     print(customer_details['street'])
 
-    question = ("+×+ PPlease enter your suburb +×+ P")
+    question = ("+×+ Please enter your suburb +×+ ")
     customer_details['suburb'] = not_blank(question)
 
     print(customer_details['suburb'])
