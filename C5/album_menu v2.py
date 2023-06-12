50362bbfcf7b5e00040f429cbc73570bc24192b9
KaitlinEus <131826234+KaitlinEus@users.noreply.github.com>
2023-06-12 14:02:38 +1200

    changed names of variables
    changed them to contain the word album

--- C5/album_menu v2.py
+++ C5/album_menu v2.py
@@ -1,18 +1,18 @@
 # list of album names and prices
-pizza_names = ["Mini Album 'The Dream Chapter: Star'", "Mini Album 'Minisode 1 : Blue Hour' AR ver", "Mini Album 'Minisode 1 : Blue Hour' R ver", 
+album_names = ["Mini Album 'The Dream Chapter: Star'", "Mini Album 'Minisode 1 : Blue Hour' AR ver", "Mini Album 'Minisode 1 : Blue Hour' R ver", 
                "Mini Album 'Minisode 1 : Blue Hour' VR ver", "Mini Album 'The Dream Chapter : Eternity' Port ver", "Mini Album 'The Dream Chapter : Eternity' Starboard ver", 
                "Full Album 'The Dream Chapter : Magic' Sanctuary ver", "Full Album 'The Dream Chapter : Magic' Arcadia ver", 
                "Repackaged Album 'The Chapter of Chaos: Fight or Escape' Fight ver", "Repackaged Album 'The Chapter of Chaos: Fight or Escape' Escape ver", 
                "Mini Album 'Minisode 2: Thursday's Child' Hate ver", "Mini Album 'Minisode 2: Thursday's Child' End ver", "Mini Album 'Minisode 2: Thursday's Child' Mess ver", 
                "Mini Album 'The Name Chapter: TEMPTATION' Daydream ver", "Mini Album 'The Name Chapter: TEMPTATION' Nightmare ver", 
                "5th Mini Album 'The Name Chapter: TEMPTATION' Farewell ver"]
-pizza_prices = [59.99, 59.99, 59.99, 59.99, 59.99, 59.99, 59.99, 59.99, 64.99, 64.99, 58.99, 58.99, 58.99, 45.99, 45.99, 45.99]
+album_prices = [59.99, 59.99, 59.99, 59.99, 59.99, 59.99, 59.99, 59.99, 64.99, 64.99, 58.99, 58.99, 58.99, 45.99, 45.99, 45.99]
 
 def menu():
-        num_pizzas = 16
+        num_albums = 16
 
-        for count in range (num_pizzas):
-            print("{} {} ${:.2f}".format(count+1, pizza_names[count], pizza_prices[count]))
+        for count in range (num_albums):
+            print("{} {} ${:.2f}".format(count+1, album_names_names[count], album_prices[count]))
 
 menu()
 
