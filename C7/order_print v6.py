11992a3fa66c8b43972c671fd603fd2d7157fd8e
KaitlinEus <131826234+KaitlinEus@users.noreply.github.com>
2023-07-17 14:51:24 +1200

    changed how the printed data looked
    added +×+

--- C7/order_print v6.py
+++ C7/order_print v6.py
@@ -9,18 +9,18 @@ customer_details = {'name': 'Denzelle', 'phone': '1234567890', 'house': '1a', 's
 def print_order():
     total_cost = sum(order_cost)
 
-    print("Customer Details")
-    print(f"Customer Name: {customer_details['name']} \nCustomer Phone Number: {customer_details['phone']} \nCustomer Address: {customer_details['house']} {customer_details['street']} {customer_details['suburb']}")
+    print("+×+ Customer Details +×+")
+    print(f"Customer Name: +×+  {customer_details['name']}  +×+ \nCustomer Phone Number: +×+  {customer_details['phone']}  +×+ \nCustomer Address: +×+  {customer_details['house']} {customer_details['street']} {customer_details['suburb']}  +×+")
     print()
 
-    print("Order Details")
+    print("+×+ Order Details +×+")
     count = 0
     for item in order_list:
-        print("Ordered: {} Cost: ${:.2f}".format(item, order_cost[count]))
+        print("Ordered: +×+  {}  +×+ Cost: +×+  ${:.2f}  +×+".format(item, order_cost[count]))
         count = count+1
     print()
         
-    print("Total Cost")
-    print(f"${total_cost:.2f}")
+    print("+×+ Total Cost +×+")
+    print(f"+×+  ${total_cost:.2f}  +×+")
 
 print_order()
