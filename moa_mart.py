70bb990b192bacc571bea355761510a3705251b6
KaitlinEus <131826234+KaitlinEus@users.noreply.github.com>
2023-07-31 18:37:35 +1200

    Integrated component 8 into the main program
    made it into a function

--- moa_mart.py
+++ moa_mart.py
@@ -222,6 +222,37 @@ def print_order(del_collect):
     print(f"+×+  ${total_cost:.2f}  +×+")
 
 
+# Ability to cancel or proceed with order
+
+def confirm_cancel():
+
+    print("+×+ Please confirm your order +×+")
+
+    print("+×+ To confirm, please enter 1 +×+")
+    print("+×+ To cancel, please enter 2 +×+")
+
+    while True: 
+        try:
+            confirm = int(input("+×+ Please enter a number +×+ "))
+            if confirm >= 1 and confirm <= 2:
+
+                if confirm == 1:
+                    print("+×+  Order confirmed  +×+")
+                    print("+×+ Your order is now being prepared +×+")
+                    break
+
+                elif confirm == 2:
+                    print("+×+  Order cancelled  +×+")
+                    print("+×+ You can restart or exit the BOT +×+")
+                    break
+            else: 
+                print("+×+ Number must be 1 or 2 +×+ ")
+
+        except ValueError:
+            print("+×+ That is not a valid number +×+")
+            print("+×+ Please enter 1 or 2 +×+ ")
+            
+
 
 
 # Main function
@@ -239,8 +270,7 @@ def main():
     menu()
     order_albums()
     print_order(del_collect)
+    confirm_cancel()
 
 
 main()
-
-
